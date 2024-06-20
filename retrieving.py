781168e46fc63e7c4b7e0cefe4d36aab55eef7c4
RPfrontiere <riccardo.petruzziello@frontiere.io>
2024-06-20 10:27:22 +0200

    new Retriever module

--- retrieving.py
+++ retrieving.py
@@ -7,27 +7,23 @@ from langchain.storage import InMemoryStore
 class Retriever:
 	def __init__(self, docs):
 		self.docs = docs
+		self.vectorstore = self.embedding()
+		self.child_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=25, add_start_index=True)
+		self.parent_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, add_start_index=True)
 	
 	def embedding(self):
 		sentence_transformer_ef = SentenceTransformerEmbeddings(model_name = "all_MiniLM-L6-v2")	
 		vectorstore = Chroma(collection_name = "full_documents", embedding_function=sentence_transformer_ef)
 		return vectorstore
-	
-	def splitter(self):
-		child_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=25, add_start_index=True)
-		parent_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, add_start_index=True)
 
-		return child_splitter,parent_splitter
 		
 	def get_retriever(self):
-		vectorstore = self.embedding()
-		child_splitter,parent_splitter = self.splitter()
 		store = InMemoryStore()
 		retriever = ParentDocumentRetriever(
-    vectorstore=vectorstore,
+    vectorstore=self.vectorstore,
     docstore=store,
-    child_splitter=child_splitter,
-    parent_splitter=parent_splitter,
+    child_splitter=self.child_splitter,
+    parent_splitter=self.parent_splitter,
 )
 
 		retriever.add_documents(self.docs)
