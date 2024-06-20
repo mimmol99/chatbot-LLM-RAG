bc88423322b34518a0472c6063675903724c2978
RPfrontiere <riccardo.petruzziello@frontiere.io>
2024-06-20 10:15:36 +0200

    Load and Retriever module

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from gui import GUI
 
 def main():
     documents_path = "./PDF_FILES"
-    docs = Loader(documents_path)
-    retriever = Retriever(docs)
+    docs = Loader(documents_path).load_documents()
+    retriever = Retriever(docs).get_retriever()
     GUI(retriever)
     
 if __name__ == "__main__":
